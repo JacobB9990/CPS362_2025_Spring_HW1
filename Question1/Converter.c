7f9bc2de923d3257c5dd80d019440858992b4916
Adithya-hv <adithyahv04@gmail.com>
2025-02-14 18:44:55 -0500

    Checks if its a double or decimal

--- Question1/Converter.c
+++ Question1/Converter.c
@@ -1,10 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdint.h>
-#include <ctype.h>
 
 #define MAX_INPUT_SIZE 256
+
+void removeNewline(char *str) {
+    size_t length = strlen(str);
+    if (length > 0 && str[length - 1] == '\n') {
+        str[length - 1] = '\0';
+    }
+}
+
+int isDecimalOrDouble(const char *str) {
+    int dotCount = 0; 
+    size_t length = strlen(str);
+
+    if (length == 0) return 0; //empty string 
+
+
+    for (size_t i = 0; i < length; i++) {
+        if (str[i] == '.') {
+            dotCount++;
+            if (dotCount > 1) return 0; // More than one dot
+        } else if (!isdigit(str[i])) {
+            return 0; //not a digit or a dot
+        }
+    }
+    
+    if (length == 64 && dotCount==0) return 2; // A Double
+
+    return 1; //Valid
+}
 
 int main(void) {
     char userInput[MAX_INPUT_SIZE];
@@ -15,4 +41,20 @@ int main(void) {
         printf("Error getting input\n");
         return 1;
     }
+
+    removeNewline(userInput);
+
+    switch (isDecimalOrDouble(userInput)){
+        case 1:
+            /* code */
+            break;
+    
+        case 2:
+            break;
+        default:
+            printf("Not an decimal or double, please check Input");
+            break;
+    }
+
+    return 0;
 }
